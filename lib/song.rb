d23f234bdb1ffca91b748a7ee749d39c2e5e2568
Joseph Vint <josephvint@me.com>
2018-04-17 20:26:35 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+
+class Song
+  attr_reader :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    Song.artists
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.detect{|index| index == @genre}
+  end
+
+  def self.artists
+    @@artists << @artist if @@artists.detect{|index| index == @artist}
+  end
+
+  def genre_count
+
+  end
+
+  def artist_count
+
+  end
+
+end
